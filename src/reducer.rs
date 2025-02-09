23bbd3d288d80e6cd2172e4fd708858acc45afbb
Kaoru Nishikawa <kq.nishikawa@gmail.com>
2025-02-09 17:44:47 +0900

    Add reducer module

---
+++ src/reducer.rs
@@ -0,0 +1,8 @@
+pub fn reduce(data: Vec<(f64, f64, f64)>, az: f64, el: f64, rot: f64) -> Vec<(f64, f64)> {
+    // TODO: Implement reducer
+    let reduced = data
+        .iter()
+        .map(|(x, y, z)| return (*x * az, *y * el))
+        .collect();
+    reduced
+}
